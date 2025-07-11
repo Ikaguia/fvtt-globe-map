e29cd79670e69c46bd03f5328ca53209d22ec955
Ikaguia <Ikaguia@users.noreply.github.com>
2025-07-11 00:14:14 -0300

    Update wiki.js

--- scripts/markers/wiki.js
+++ scripts/markers/wiki.js
@@ -1,8 +1,24 @@
 import { Marker } from "./marker.js";
 
 export class WikiLinkMarker extends Marker {
+	reset() {
+		super.reset();
+		this.hovering = false;
+	}
+
 	// Getters
-	get layerIDs() { return ["location-labels", "location-icons"]; }
+	get layerIDs() {
+		return [
+			"location-icons",
+			"location-labels",
+			"symbol_labels",
+			"symbol_line-labels",
+			"symbol_province-labels",
+			"symbol_nation-labels",
+			"symbol_subregion-labels",
+			"symbol_region-labels",
+		];
+	}
 
 	// Event handlers
 	onClick(event, properties) {
@@ -12,5 +28,26 @@ export class WikiLinkMarker extends Marker {
 		const url = properties.link;
 		console.log(`Opening Pathfinder Wiki link: ${url}`);
 		window.open(url, "_blank");
+	}
+	onMouseMove(event, hovering) {
+		const alt = event?.originalEvent?.altKey;
+		const hasLink = hovering?.filter?.(f => f.properties.link).length;
+		if (alt && hasLink) this.hover();
+		else this.unhover();
+	}
+	onLeaveMap() { this.unhover(); }
+
+	// Helper Functions
+	hover() {
+		if (!this.hovering) {
+			this.hovering = true;
+			this.map.getCanvas().style.cursor = "pointer";
+		}
+	}
+	unhover() {
+		if (this.hovering) {
+			this.hovering = false;
+			this.map.getCanvas().style.cursor = "";
+		}
 	}
 }
